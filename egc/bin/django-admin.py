f124678f9b038455e01179fa728f3495f05754b2
AntonioJesusGraciaNieto <curiosidadcompacta@gmail.com>
2021-02-09 17:11:39 +0100

    Añadido fichero travis

---
+++ egc/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/antoniojesus/Documentos/p_e_1819_1230/EGC-1819-1230-antgarnie1/egc/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
